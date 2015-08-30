8da0076361885ac924b5dcc7b9b5eb71f9ee81ef
ParamieJayasinghe <paramyj20@gmail.com>
2015-08-30 21:18:29 +0530

    login registration fixed

--- application/views/includes/loginform.php
+++ application/views/includes/loginform.php
@@ -1,3 +1,69 @@
+<?php
+require_once '../configs/core/init.php';
+
+if(Input::exists()){
+	if(Token::check(Input::get('token'))){
+		//$validate= new Validate();
+		//$validation=$validate->check($_POST,array(
+			//'User_Name'=>array('required'=>true),
+			//'Password'=>array('required'=>true)
+					
+			//));
+		
+		//if($validation->passed()){
+			$user=new User();
+			
+			$remember = (Input::get('remember')==='on') ? true : false;
+			
+			$login=$user->login(Input::get('User_Name'),Input::get('Password'),$remember);
+			
+			if($login){
+				redirect::to('index.php');
+				}else{
+					echo '<p> Sorry logging in failed.</p>';
+					}
+		}else{
+			foreach($validation->errors() as $error){
+				echo $error,'<br>';
+				
+				}
+				
+				}
+		}
+	//}
+
+?>
+<?php
+
+if(Session::exists('home')){
+	
+	echo '<p>' . Session::flash('home') . '</p>';
+	}
+	
+$user =new User();
+if($user->isLoggedIn()){
+	$name =  escape($user->data()->User_Name);	
+	
+	
+
+	if($user->hasPermission('admin')){
+		redirect::to('user/adminhome.php?name='.$name);
+		}else{
+			redirect::to('user/userhome.php?name='.$name);
+
+			}
+
+	}
+	
+		?>
+		
+		
+
+ 
+
+	
+
+
           <div class="form-top">
                                 <div class="form-top-left">
                                     <h3>Please sign in</h3>
@@ -12,32 +78,35 @@
 
                                     <div class="form-group">
                                         <label class="sr-only" for="form-first-name">User name</label>
-                                        <input type="text" name="username" placeholder="User name" class="form-first-name form-control" id="form-first-name">
+                                        <input type="text" name="User_Name" id="User_Name" placeholder="User name" class="form-first-name form-control" id="form-first-name">
                                     </div>
 
                                     <div class="form-group">
                                         <label class="sr-only" for="form-last-name">Password</label>
-                                        <input type="text" name="password" placeholder="Password" class="form-last-name form-control" id="form-last-name">
+                                        <input type="text" name="Password" id="Password" placeholder="Password" class="form-last-name form-control" id="form-last-name">
                                     </div>
 
                                     <div class="row">
                                         <div class="col-xs-3 col-sm-4">
                                             <div class="checkbox">
                                                 <label>
-                                                    <input type="checkbox" value="remember-me"> Remember me
+                                                    <input type="checkbox" name="remember" id="remember"> Remember me
                                                 </label>
                                              </div>
                                         </div>
                                          <div class="col-xs-8 col-sm-6 text-left">
                                                 <p class="help-block">( if this is a private computer )</p> 
                                         </div>
+                                        
                                     </div>
+                                    <input type="hidden" name="token" value="<?php echo Token::generate();?>" >
 
                                     <div class="row">
                                         <div class="col-xs-6">
                                            <button type="submit" class="btn full">Sign in!</button>
                                         </div>
                                          <div class="col-xs-6 ">
+                                         	
                                             <button id = "registration" class="btn full ">Sign up now!</button>
                                         </div>
                                     </div>
--- application/views/includes/registrationform.php
+++ application/views/includes/registrationform.php
@@ -1,3 +1,46 @@
+<?php
+require_once '../configs/core/init.php';
+
+if(Input::exists()){
+	if(Token::check(Input::get('token'))){
+		
+		
+		//if($validation->passed()){
+			$user=new User();
+			
+			$salt = Hash::salt(32);
+			
+			try{
+				$user->create(array(
+				'First_Name'=>Input::get('First_Name'),
+				'Last_Name'=>Input::get('Last_Name'),
+				'User_Name'=>Input::get('User_Name'),
+				'E_mail'=>Input::get('E_mail'),
+				'Password'=>Hash::make(Input::get('Password'),$salt),
+				'group'=>1,
+				'salt'=>$salt
+				
+				
+				));
+				
+				Session::flash('home','You have been registered and can now log in!');
+				redirect::to('index.php');
+				
+			}catch(Exception $e){
+				die($e->getMessage());
+			
+			}
+			}else{
+				foreach($validation->errors() as$error){
+					echo $error,'<br>';
+					
+					}
+				}
+	}
+	//}
+
+?>
+
       <div class="form-top">
                                 <div class="form-top-left">
                                     <h3>Sign up now</h3>
@@ -12,28 +55,33 @@
 
                                     <div class="form-group">
                                         <label class="sr-only" for="form-first-name">First name</label>
-                                        <input type="text" name="firstName" placeholder="First name..." class="form-first-name form-control" >
+                                        <input type="text" name="First_Name" id="First_Name" placeholder="First name..." class="form-first-name form-control" >
                                     </div>
 
                                     <div class="form-group">
                                         <label class="sr-only" for="form-last-name">Last name</label>
-                                        <input type="text" name="lastName" placeholder="Last name..." class="form-last-name form-control" >
+                                        <input type="text" name="Last_Name" id="Last_Name" placeholder="Last name..." class="form-last-name form-control" >
+                                    </div>
+                                    <div class="form-group">
+                                        <label class="sr-only" for="form-first-name">User name</label>
+                                        <input type="text" name="User_Name" id="User_Name" placeholder="User name..." class="form-first-name form-control" >
                                     </div>
 
                                     <div class="form-group">
                                         <label class="sr-only" for="form-email">Email</label>
-                                        <input type="text" name="email" placeholder="Email..." class="form-email form-control" >
+                                        <input type="text" name="E_mail" id="E_mail" placeholder="Email..." class="form-email form-control" >
                                     </div>
 
                                     <div class="form-group">
                                         <label class="sr-only" for="form-email">Email</label>
-                                        <input type="password" name="password" placeholder="password..." class="form-email form-control" id="password">
+                                        <input type="password" name="Password" id="Password" placeholder="password..." class="form-email form-control" id="password">
                                     </div>
 
                                     <div class="form-group">
                                         <label class="sr-only" for="form-email">Email</label>
-                                        <input type="password" name="password2" placeholder="password again..." class="form-email form-control" >
+                                        <input type="password" name="password_again" id="password_again" placeholder="password again..." class="form-email form-control" >
                                     </div>
+                                    <input type="hidden" name="token" value="<?php echo Token::generate();?>" >
 
                                    <div class="form-group">
                                         <label>
