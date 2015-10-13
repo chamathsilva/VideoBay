8b3e7f815e540fd48476d56c4690e1ed817eff35
ParamieJayasinghe <paramyj20@gmail.com>
2015-10-13 19:52:55 +0530

    new bulk registration

---
+++ application/views/registration/api.php
@@ -0,0 +1,6 @@
+<form action="uploadfile.php" method="post" enctype="multipart/form-data">
+   <p>
+      <label for="file">Select a file:</label> <input type="file" name="fileToUpload" id="fileToUpload"> <br />
+      <input type="submit" value="Upload File" name="submit"/>
+   <p>
+</form>
