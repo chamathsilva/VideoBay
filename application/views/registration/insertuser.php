145f07d6de1a3947b651ab9702189e6abeee5260
ParamieJayasinghe <paramyj20@gmail.com>
2015-11-03 12:09:59 +0530

    validate user

--- application/views/registration/insertuser.php
+++ application/views/registration/insertuser.php
@@ -1,40 +1,86 @@
 <?php
   
-require_once '../../configs/core/init.php';
+require_once '/../../configs/core/init.php';
 function insertUser($datalist=array()){
-	
+	$flag=0;
 				$First_Name = $datalist[0] ;
 				$Last_Name  =$datalist[1];
 				$User_Name =$datalist[2];
 				$E_mail   =$datalist[3];
 				$Password  =$datalist[4];
 				$usertype =$datalist[5];
+		
+	if ($usertype==4){
+					
+					$flag=5;
+					
+					
+					//return $flag;
+					
+					}
+					
+	else if($usertype==3){
+		
+					$flag=5;
+					
+					//return $flag;
+					
+		
+		}
+		
+	else {
+		
+			$flag=2;
+			
+			//return $flag;
+				}
+				
+				
+	//return $flag;
+	
+	
+	//}
+
+//function insertUser($datalist=array()){
+	
+	
+				
+	
+
+	if($flag==5){	
 				
 		
-	$user=new User();
-			
-			$salt = Hash::salt(32);
-			
-			try{
-				$user->create(array(
-				'First_Name'=>$First_Name,
-				'Last_Name'=>$Last_Name,
-				'User_Name'=>$User_Name,
-				'E_mail'=>$E_mail,
-				'Password'=>Hash::make(Input::get($Password),$salt),
-				'group'=>$usertype,
-				'salt'=>$salt
+		$user=new User();
 				
+				$salt = Hash::salt(32);
 				
-				));
+				try{
+					$user->create(array(
+					'First_Name'=>$First_Name,
+					'Last_Name'=>$Last_Name,
+					'User_Name'=>$User_Name,
+					'E_mail'=>$E_mail,
+					'Password'=>Hash::make(Input::get($Password),$salt),
+					'group'=>$usertype,
+					'salt'=>$salt
+					
+					
+					));
+					
+					
+					
+				}catch(Exception $e){
+					die($e->getMessage());
 				
-				
-				
-			}catch(Exception $e){
-				die($e->getMessage());
-			
-			}
+				}
+		
 	}
+	else if ($flag==2){
+		//echo "error";
+		}
+
+//}
+}
 
 
 ?>
