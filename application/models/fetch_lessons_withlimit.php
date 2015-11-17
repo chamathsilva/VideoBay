c30b9eecb774ff2b0ef66669937d4752d81f1149
Chamath Silva <mbckchamathsilva@gmai.com>
2015-11-18 04:09:04 +0530

    up date watch next function

--- application/models/fetch_lessons_withlimit.php
+++ application/models/fetch_lessons_withlimit.php
@@ -1,7 +1,16 @@
 <?php
 require("Db.class.php");
 $db = new Db();
-$lessons = $db->query("SELECT * FROM lesson");
+
+
+if(isset($_POST["id"])){
+    $id = $_POST["id"];
+    $lessons = $db->query("SELECT * FROM `lesson` WHERE `lesson_id` <>".$id);
+}
+
+
+
+
 
 foreach ($lessons as $row) {
     $id = $row['lesson_id'];
@@ -10,13 +19,13 @@ foreach ($lessons as $row) {
     $src_path = '../../../data/uploaded_lessons/'.$id.'/slides/1.jpg';
 
     echo '<li class="media">';
-    echo '<div class="col-xs-7">';
+    echo '<div class="col-lg-7 col-md-7 col-xs-7">';
     echo '<a class="pull-left" href="../lessonsplay/lessonPlay2.php?id='."$id".'" >';
     echo '<img class="img-responsive" src='."$src_path".' alt="">';
     echo '</div>';
 
     echo '<p class="media-heading">'."$name".'</p>';
-    echo '<p class="by-author">'."By "."$lecture".'</p>';
+    //echo '<p class="by-author">'."By "."$lecture".'</p>';
     echo'</a>';
 
     echo '<li>';
